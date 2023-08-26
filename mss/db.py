697822256ccf8c1625c9223944f34c87cf8eef89
qus0in <qus0in@gmail.com>
2023-08-26 14:58:42 +0900

    노션에 score update

--- mss/db.py
+++ mss/db.py
@@ -1,15 +1,81 @@
 import pandas as pd
 import requests
 import os
+from io import BytesIO
 
 class FinanceDB:
-    def get_etfs():
+    prices = {}
+    fibonacci = (8, 13, 21, 34, 55)
+    names = None
+
+    @classmethod
+    def get_etfs(cls):
         URL = 'https://finance.naver.com/api/sise/etfItemList.nhn'
         data = requests.get(URL).json()\
             .get('result').get('etfItemList')
-        mapper = lambda x: {'itemcode': x.get('itemcode'), 'itemname': x.get('itemname')}
-        df = pd.DataFrame(list(map(mapper, data))).rename(columns={'itemname': 'NAME'})
+        mapper = lambda x: {
+            'itemcode': x.get('itemcode'),
+            'itemname': x.get('itemname')}
+        df = pd.DataFrame(list(map(mapper, data)))\
+            .rename(columns=dict(
+                itemcode='SYMBOL',
+                itemname='NAME'))
+        cls.names = df.copy()
         return df
+    
+    @classmethod
+    def get_price(cls, symbol):
+        if symbol in cls.prices:
+            return cls.prices[symbol]
+        URL = 'https://api.finance.naver.com/siseJson.naver'
+        params = dict(
+            symbol=symbol, requestType=1,
+            startTime='20230101', endTime='20991231', timeframe='day'
+        )
+        content = requests.get(URL, params).content
+        with BytesIO(content) as byte_io:
+            df = pd.read_csv(byte_io).iloc[:-1, 0:5]
+        df.columns = ['Date', 'Open', 'High', 'Low', 'Close']
+        df.Date = df.Date.str.extract(r'(\d{8})')
+        df.Date = pd.to_datetime(df.Date).dt.date
+        df.set_index('Date', inplace=True)
+        cls.prices[symbol] = df
+        return df
+    
+    @classmethod
+    def get_momentum(cls, symbol):
+        price = cls.get_price(symbol)
+        # 8, 13, 21, 34, 55
+        return (
+            sum([price.Close.rolling(f)\
+            .apply(lambda x: (x[-1] / x[0]))\
+            .sub(1).div(f).add(1).pow(252).sub(1)
+            for f in cls.fibonacci])\
+            .div(len(cls.fibonacci))\
+            .round(3).iloc[-1])
+
+    @classmethod
+    def get_risk(cls, symbol):
+        price = cls.get_price(symbol)
+        concat = lambda x, y: pd.concat([x, y], axis=1)
+        th = concat(price.High, price.Close.shift(1)).max(axis=1)
+        tl = concat(price.Low, price.Close.shift(1)).min(axis=1)
+        tr = th - tl
+        atr = tr.ewm(max(cls.fibonacci)).mean()
+        aatr = atr / price.Close
+        return aatr.round(3).iloc[-1]
+    
+    @classmethod
+    def get_score(cls, symbols):
+        cls.get_etfs()
+        return pd.DataFrame([dict(
+            ID=ID, SYMBOL=symbol,
+            NAME=cls.names.loc[cls.names.SYMBOL == symbol, 'NAME'].iloc[-1],
+            MOMENTUM=cls.get_momentum(symbol),
+            RISK=cls.get_risk(symbol),
+        ) for ID, symbol in symbols])\
+        .sort_values('MOMENTUM', ascending=False)\
+        .reset_index(drop=True)
 
 class NotionDB:
     def __init__(self) -> None:
@@ -51,3 +117,11 @@ class NotionDB:
         self.db_id = os.getenv('mss_item_db_id')
         if not self.db_id:
             raise Exception('NO DB ID') 
+        
+    @classmethod
+    def text_mapper(cls, content):
+        return {'rich_text': [{'text':{'content': content}}]}
+
+    @classmethod
+    def number_mapper(cls, value):
+        return {'number': value}
--- mss/main.py
+++ mss/main.py
@@ -1,17 +1,19 @@
 import db
 
+# 3, 5, 8, 13, 21, 34, 55, 89
 def handler(event=None, context=None):
-    notionDB = db.NotionDB()
+    notionDB = db.NotionDB() 
     symbols = notionDB.get_symbols()
-    etfs = db.FinanceDB.get_etfs().rename(columns={'itemcode':'SYMBOL'})
-    df = symbols.merge(etfs, how='inner')
-    print(df)
-    for _, row in df.iterrows():
-        notionDB.update_properties(
-            row.ID,
-            {'NAME': {'rich_text': [{'text':{'content': row.NAME}}]}}
-        )
-    # return df.to_json()
+    # print(df)
+    df_score = db.FinanceDB.get_score(
+        [(row.ID, row.SYMBOL) for _, row in symbols.iterrows()])
+    print(df_score)
+    # 노션 DB 업데이트
+    for _, row in df_score.iterrows():
+        notionDB.update_properties(row.ID, dict(
+            NAME = notionDB.text_mapper(row.NAME),
+            MOMENTUM = notionDB.number_mapper(row.MOMENTUM),
+            RISK = notionDB.number_mapper(row.RISK),))
 
 if __name__ == '__main__':
     handler()
