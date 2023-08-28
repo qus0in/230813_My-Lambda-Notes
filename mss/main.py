e9414838eba70732c4be22442ea9376c3cbf2aa7
qus0in <qus0in@gmail.com>
2023-08-28 21:59:24 +0900

    buffer 적용

--- mss/main.py
+++ mss/main.py
@@ -12,7 +12,7 @@ def handler(event=None, context=None):
         [(row.ID, row.SYMBOL) for _, row in symbols.iterrows()])
     print(df_score)
     df_score['SCORE'] = df_score.apply(lambda row: floor(min(risk / row.RISK, 1) * row.MOMENTUM * 1000), axis=1).div(1000)
-    df_score['UNIT'] = df_score.apply(lambda row: floor(min(risk / row.RISK, 1) * budget / 5 / 10000), axis=1).mul(10000)
+    df_score['UNIT'] = df_score.apply(lambda row: floor(min(risk / row.RISK, 1) * budget / 5 / 100000), axis=1).mul(100000)
     df_score.sort_values('SCORE', ascending=False, inplace=True)
     free_risk = df_score.query('SYMBOL == "357870"').iloc[-1].SCORE
     relative_momentum = df_score.head(6).iloc[-1].SCORE
